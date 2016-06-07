362f7e2960e5a19b3f19b5119f09e9d8abb0c709
Moses Zhu <Moses.Zhu@qisda.com>
2016-06-07 21:11:06 +0800

    Init

---
+++ chp2/js/spa.shell.js
@@ -0,0 +1,22 @@
+/**
+ * Created by DELL on 2016/6/4.
+ */
+spa.shell = (function () {
+    var configMap = {
+        main_html : String()
+            + '<div class="spa-shell-head">'
+            +   '<div class="spa-shell-head-logo"></div>'
+            +   '<div class="spa-shell-head-acct"></div>'
+            +   '<div class="spa-shell-head-search"></div>'
+            + '</div>'
+            + '<div class="spa-shell-main">'
+            +   '<div class="spa-shell-main-nav"></div>'
+            +   '<div class="spa-shell-main-content"></div>'
+            + '</div>'
+            + '<div class="spa-shell-foot"></div>'
+            + '<div class="spa-shell-chat"></div>'
+            + '<div class="spa-shell-modal"></div>'
+    },
+        stateMap = {},
+        jqueryMap = {};
+}());
