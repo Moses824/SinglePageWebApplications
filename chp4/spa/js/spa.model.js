dafd0256ac2d44c08d5424d2db7c62556a7a8c07
Moses Zhu <moses_z_a@163.com>
2016-06-10 23:04:13 +0800

    add chp4 files

---
+++ chp4/spa/js/spa.model.js
@@ -0,0 +1,6 @@
+/**
+ * Created by DELL on 2016/6/10.
+ */
+spa.model = (function () {
+   return {};
+}());
---
+++ chp4/spa/js/spa.util.js
@@ -0,0 +1,6 @@
+/**
+ * Created by DELL on 2016/6/10.
+ */
+spa.util = (function () {
+   var makeError, setConfigMap;
+}());
