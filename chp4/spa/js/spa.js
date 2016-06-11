b8677530b1c0ce27f449dec1771674a9c26ef14f
Moses Zhu <moses_z_a@163.com>
2016-06-11 16:21:41 +0800

    init chp4

--- chp4/spa/js/spa.js
+++ chp4/spa/js/spa.js
@@ -1,3 +1,10 @@
 /**
  * Created by DELL on 2016/6/10.
  */
+var spa = (function () {
+    var initModule = function ($container) {
+        spa.shell.initModule($container);
+    };
+
+    return {initModule : initModule}
+} ());
